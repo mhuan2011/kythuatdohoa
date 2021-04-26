beeb81d44eaedac32599f00e0f3436d28cf848c7
mhuan2011 <minhhuan1100@gmail.com>
2021-04-26 12:49:23 +0700

    Update layout

--- DOAN/main.cpp
+++ DOAN/main.cpp
@@ -17,30 +17,31 @@ void giaoDien(){
 	rectangle(50,20,200,60); //2d
 	rectangle(50,80,200,120);  //3d
 	setcolor(0);
-	settextstyle(1, 0, 3);
+	settextstyle(8, 0, 3);
 	outtextxy(100, 28, "2D");
 	outtextxy(100, 88, "3D");
 
 
 	rectangle(0,0,250,150); // khung nhap lieu
 	rectangle(20,180,230,450);
-	settextstyle(1, 0, 1);
+	settextstyle(8, 0, 1);
 	outtextxy(35, 170, "Parameters");
 	
 	rectangle(250,0,890,590); //khung thao tac
 	
-	rectangle(70,520,180,550); //clear button
-	setcolor(12);
-	outtextxy(90, 525, "Clear");
 	
+	setfillstyle(1, 4);
+	setbkcolor(4);	
+	bar(70,520,180,550); //clear button
+	setcolor(15);
+	
+	outtextxy(95, 525, "Clear");
+	setbkcolor(15);
 }
 
 void getMouseClick(){
 	int x, y;        // Coordinates of the mouse click
 
-    int divisor;     // Divisor for the length of a triangle side
-
-    divisor = 2;
 
     while (1)
 
@@ -50,20 +51,34 @@ void getMouseClick(){
 
 	    if(x > 50 && x < 200 && y > 20 && y <60){ //xua li 2d
 	    	setcolor(GREEN);
-			settextstyle(1, 0, 3);
+			settextstyle(8, 0, 3);
 			outtextxy(100, 28, "2D");
 			setcolor(0);
-			settextstyle(1, 0, 3);
 			outtextxy(100, 88, "3D");
+			
+			setfillstyle(1, 0);
+			bar(250,0,890,590);
+			
+			settextstyle(8, 0, 1);
+			setcolor(0);
+			outtextxy(50, 200, "Height: ");
+			
+			
+			//
+			
+//			gotoXY(45, 170);
+//			int x; cin >>x;
 		}
 	    
 	    if(x > 50 && x < 200 && y > 80 && y <120){ //xu li 3d
 	    	setcolor(GREEN);
-			settextstyle(1, 0, 3);
+			settextstyle(8, 0, 3);
 			outtextxy(100, 88, "3D");
 			setcolor(0);
-			settextstyle(1, 0, 3);
+			settextstyle(8, 0, 3);
 			outtextxy(100, 28, "2D");
+			setfillstyle(1, 2);
+			bar(250,0,890,590);
 		}
 		
 		if(kbhit()) break;
